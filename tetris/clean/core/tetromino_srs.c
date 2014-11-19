38414e94a558dc1d96f38288c41a2a2a60556116
Cedric Doucet <cedric.doucet@inria.fr>
2014-11-19 16:34:43 +0100

    Fix wrong rotation for tetromino L

--- tetris/clean/core/tetromino_srs.c
+++ tetris/clean/core/tetromino_srs.c
@@ -48,7 +48,7 @@ static TetrominoAllRotations tetrominoJAllRotations = {
 };
 
 static TetrominoAllRotations tetrominoLAllRotations = {
-  { {2,0}, {1,0}, {1,1}, {2,1} },
+  { {0,2}, {1,0}, {1,1}, {1,2} },
   { {0,1}, {1,1}, {2,1}, {2,2} },
   { {1,0}, {1,1}, {1,2}, {2,0} },
   { {0,0}, {0,1}, {1,1}, {2,1} }
